083010c655c0ea9c91e17440e9ee1190906d97d2
davinci360 <jasonbrowntime@gmail.com>
2021-07-12 21:40:11 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let x = 0; x < namesArray.length; x++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[x]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+  
+
+  //countdown to apocalypse j/k >:P
+function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
